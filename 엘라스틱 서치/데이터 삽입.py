ba7e89a94cb1f6e0de223ebc3d166d0cd00fc04f
hwanu <sossont1@gmail.com>
2021-07-26 16:00:02 +0900

    엘라스틱 인덱싱

--- 엘라스틱 서치/데이터 삽입.py
+++ 엘라스틱 서치/데이터 삽입.py
@@ -1,20 +1,24 @@
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 
+
+## conviction이 숫자값과 inf값 두 가지를 갖기 때문에 자꾸 오류가 발생한다. inf값을 먼저 처리해주어야 할 것 같다.
 es = Elasticsearch()
 es.info()
 
-data = [    # 삽입할 데이터. 예시로 조건절, 결과절, support 3값만 넣어두었다. id는 인덱스를 넣어줌.
-    {
-        "_index": "as_rule",
-        "_type": "object",
-        "_id": x[3],
-        "_source": {
-            "antecedents": x[0],
-            "consequents": x[1],
-            "support": x[2]
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+es = Elasticsearch('localhost:9200')
+es.info()
+ndict = real_df.to_dict('records') # 딕셔너리로 만들어준다.
+for i in range(0,len(real_df)):
+    docs = {
+            'antecedents': ndict[i]['antecedents'],
+            'consequents': ndict[i]['consequents'],
+            'support': round(ndict[i]['support'],7),
+            'confidence': round(ndict[i]['confidence'],7),
+            'leverage': round(ndict[i]['leverage'],7),
+            'lift': round(ndict[i]['lift'],7)
         }
-    }
-    for x in zip(real_df['antecedents'],real_df['consequents'],real_df['support'],real_df.index)
-]
-helpers.bulk(es, data)
+    res = es.index(index='as_rule', doc_type="result",body=docs)
+
